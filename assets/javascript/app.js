9942b59ae7d3a2932675b735f7f4e14e0752497f
Grant Stoltz <grant@grantstoltz.com>
2017-12-12 21:01:59 -0700

    Functioning API call and rendering images to HTML

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -15,26 +15,72 @@ $(document).ready(function(){
         xappToken = res.token; 
     });
 
-    var url = "https://api.artsy.net:443/api/artists/"
+    var url = "https://api.artsy.net/api/"
 
-    var artistName = "4d8b92b34eb68a1b2c0003f4"
-
-    var artistQuery = url + artistName
-
-    console.log(artistQuery)
+    var artistId;
+    
 
     //On click listener for arist search
     $("#submit").on("click", function(event){
         event.preventDefault();
+
+    var artistName = $("#artist-name").val().trim()
+
+    var artistNameQuery = url + "search?q=" + artistName
+
+$.ajax({
+    url: artistNameQuery,
+    method: 'GET',
+    beforeSend: function(xhr){xhr.setRequestHeader('X-Xapp-Token', xappToken);}
+    }).done(function(data){
+        console.log(data)
+        artistId = data._embedded.results[0]._links.self.href.split("/").pop();     
+        artistIdQueryFunc()
+        console.log(artistId)
+    })
+  
+})
+    
+function artistIdQueryFunc () {
+    var artistIdQuery = url + "artworks?artist_id=" + artistId
     $.ajax({
-        url: artistQuery,
+            url: artistIdQuery,
+            method: 'GET',
+            beforeSend: function(xhr){xhr.setRequestHeader('X-Xapp-Token', xappToken);}
+    }).done(function(results){
+            console.log(results)
+
+            for (var i=0; i < results._embedded.artworks.length; i++){
+           
+            var arrayId = results._embedded.artworks[i].id
+            
+            getArtworkUrl(arrayId)
+            }
+    })
+}
+
+function getArtworkUrl (id) {
+    var artworkQuery = url + "/artworks/" + id
+    $.ajax({
+        url: artworkQuery,
         method: 'GET',
         beforeSend: function(xhr){xhr.setRequestHeader('X-Xapp-Token', xappToken);}
     }).done(function(results){
         console.log(results)
+        var image = results._links.thumbnail.href
+        console.log("image is " + image);
+        renderImages(image)
+
     })
-    })
-    
+}
+
+function renderImages(img){
+    var imgDiv = $("<img>")
+    imgDiv.attr("src", img)
+    $("#carousel").append(imgDiv)
+}
+
+
     
     //queries API for artist ID
 

fe1e4cf7291ffda6247c5ed8dc9f4a33cf27991f
Grant Stoltz <grant@grantstoltz.com>
2017-12-16 13:35:23 -0700

    Javascript and HTML changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,89 @@
+///////Google Maps API/////////
+
+//Google Maps API global variables
+var map;
+var markers = [];
+var addressLng;
+var addressLat;
+var addressFormatted;
+var addrMarker;
+var LocationString;
+
+
+//Initialize Map
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 13,
+        center: {lat: 32.2350428, lng: -110.9547842 }
+    });
+    console.log("In initmap");
+    addMarker( 32.2350428, -110.9547842, 'U of A' );
+    $("#address").val( "Tucson, AZ" );
+    };
+
+
+//Add marker function for Google maps
+function addMarker( locLat, locLng, locTitle ){
+    var marker = new google.maps.Marker({
+        position : new google.maps.LatLng( locLat, locLng ),
+        title : locTitle,
+        map : map
+    });
+    markers.push(marker);
+    };
+
+//Clears markers on map
+function clearMarkers() {
+    setMapOnAll(null);
+    markers = [];
+    }
+
+// Sets the map on all markers in the array.
+function setMapOnAll(map) {
+    for (var i = 0; i < markers.length; i++) {
+        markers[i].setMap(map);
+    }
+    }
+
+
+function displayMap( LocationString ){
+    console.log("In displayMap");
+    // Get the latitude and longitude of the Address from the Address String passes in.
+        $.get("https://maps.googleapis.com/maps/api/geocode/json?address=" + LocationString + "&key=AIzaSyCSCJJqp5sR0Q6TMspPZ_SeqnTC7iLPnmE")
+    .done(function(data){
+        var address = JSON.stringify(data);
+        console.log("Results - " + address );
+        console.log("Data Lat- " + data.results[0].geometry.location.lat);
+        console.log("Data Lng- " + data.results[0].geometry.location.lng);
+        console.log("Short Name- " + data.results[0].address_components.short_name);
+        addressLng = data.results[0].geometry.location.lng;
+        addressLat = data.results[0].geometry.location.lat;
+        addrMarker = data.results[0].address_components.short_name;
+        addressFormatted = data.results[0].formatted_address;
+        // Create the map with the lat/lng retrieved from the API.
+        map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 13,
+            center: {lat:Number(addressLat), lng:Number(addressLng) },
+        });
+        addMarker( addressLat, addressLng, addrMarker );
+        $("#galleryName").text( LocationString );
+        $("#galleryAddr").text( addressFormatted );
+    });
+}
+
 $(document).ready(function(){
 
-    //Establish global variables for API call token
+////////ARTSY API//////////
+
+//Establish global variables for API call token
 
     var clientID = 'f3a6bc6bafa647a1b773'
     var clientSecret = '301b70ba99405fc2273322fe97ae23d1'
     var tokenUrl = 'https://api.artsy.net/api/tokens/xapp_token?'
     var xappToken;
 
+    //Artsy API Token query
     function initializeQuery(){
         $.ajax({
             url: tokenUrl + "client_id=" + clientID + "&client_secret=" + clientSecret,
@@ -23,6 +100,8 @@ $(document).ready(function(){
 
     var imageArray = [];
 
+    var artistName
+
 //On click listener for arist search
     $("#submit").on("click", function(event){
 
@@ -37,7 +116,7 @@ $(document).ready(function(){
 
     function getArtistName(){
 
-        var artistName = $("#artist-name").val().trim()
+        artistName = $("#artist-name").val().trim()
         
         var artistNameQuery = url + "search?q=" + artistName
         
@@ -95,9 +174,9 @@ $(document).ready(function(){
             var id = results.id
             var medium = results.medium
             var title = results.title
-
+            
             imageArray.push({
-                id: counter,
+                arrayId: counter,
                 imgId: id,
                 imgUrl: image,
                 title: title,
@@ -126,7 +205,7 @@ $(document).ready(function(){
         var imgDiv = $("<img>")
         .attr("src", value.imgUrl)
         .attr("id", "carousel-image")
-        .attr("data", value.id)
+        .attr("data", value.arrayId)
     
 
         var aDiv = $("<a>")
@@ -150,22 +229,40 @@ $(document).ready(function(){
 
     $(".carousel").on("click", "#carousel-image", function (){
 
-         
         var imgSrc = $(this).attr("src")
-        var id = $(this).attr("data")
+        var clickId = parseInt($(this).attr("data"))
 
-        renderImage(imgSrc)
+        var artLocation;
+        var artTitle;
+        var artMedium;
+
+        for (var i = 0; i < imageArray.length; i++){
+            
+            if (imageArray[i].arrayId == clickId){
+                artLocation = imageArray[i].institution
+                artTitle = imageArray[i].title
+                artMedium = imageArray[i].medium
+            }
+
+        }
+        
+        renderImage(imgSrc, artTitle, artMedium)
+        displayMap(artLocation)
 
     })
 
-    function renderImage(img){
+    function renderImage(img, title, medium){
 
     $("#card-image").empty();
 
-        var imgDiv = $("<img>")
-            .attr("src", img)
-
+    var imgDiv = $("<img>").attr("src", img)
+    var smallerImg = $('<img>').attr('src', img)
+   
     $("#card-image").html(imgDiv)
+    $("#infoPic").html(smallerImg);
+    
+    $("#art-title").text(title)
+    $("#link").html('<a href=https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Daps&field-keywords=' + "fine%20art%20" + encodeURI(title) + "%20" + encodeURI(artistName) + '>Find on Amazon</a>')
 
     }
     
@@ -189,5 +286,7 @@ $(document).ready(function(){
     //Render image to lefthand div
 
 initializeQuery();
+
+
 
 })

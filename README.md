2fff2d0b8431266b347737c6ece3a9aa042a60c3
Abby Thoresen <akthoresen39@gmail.com>
2018-03-10 14:36:04 -0700

    final files

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # The-Art-of-Finding
 
+Description
+
+The Art of Finding application allows the user to search for images of historic artwork. It provides data about the work of art, as well as the location of the gallery where the art work is currently on display. Google Maps provides the address of the gallery. The user can browse Amazon to purchase images of the art work.
+
+Design Team
+Trish Mederos – Back end, Google Maps API
+Dameon Mendoza – Front end
+Grant Stolz – Team Lead – Back end, Artsy API
+Abby Thoresen – Front end Lead
+
+Motivation
+We all enjoy fine art (Especially Abby). 
+It allows you to locate images of historic artwork.
+You can print images from the application.
+You can purchase images of the artwork from Amazon.
+
+Results
+Team Efforts
+We began the project by using Wireframe.cc to create an initial layout of the project idea. We choose Grant as the team lead for the project and put him in charge of GitHub. We split the team into a Front End group and a Back End group. Each consisted of two members. Each team member worked on a portion of the project where they felt they had the most strength. Abby and Dameon learned Materialize CSS for the Front End design. Grant worked on the Artsy API and Trish worked on the Google Maps API in the Back End. All team members assisted in testing.
+
+Challenges
+* Abby’s biggest challenge was learning Materialize CSS. 
+
+* Dameon’s biggest challenge was making sure the application was mobile friendly and trying to ensure the media breaks worked properly.
+
+* Trish’s biggest challenge was overcoming the CORS (cross origin resource sharing) limitations of Google maps. Google Maps allows CORS, but not on client side applications. 
+
+* Grant’s biggest challenge was getting the Artsy API token authentication working.
+
+Improvements
+Add addition art archiving APIs. The Artsy API only contains public works of art. 
+It would be great to find a way to add local artists to the application.
+
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,26 +2,34 @@
 
 //Google Maps API global variables
 var map;
-var markers = [];
 var addressLng;
 var addressLat;
 var addressFormatted;
-var addrMarker;
 var LocationString;
 
 
 //Initialize Map
-
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
         zoom: 13,
         center: { lat: 32.2350428, lng: -110.9547842 }
     });
-    console.log("In initmap");
+
     addMarker(32.2350428, -110.9547842, 'U of A');
-    $("#address").val("Tucson, AZ");
+    $("#galleryName").text("");
+    $("#galleryAddr").text("");
 };
 
+//Resets the map to the entire United States
+function resetMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 3,
+        center: { lat: 37.50, lng: -95.35 }
+    });
+    
+    $("#galleryName").text("");
+    $("#galleryAddr").text("");
+};
 
 //Add marker function for Google maps
 function addMarker(locLat, locLng, locTitle) {
@@ -30,50 +38,31 @@ function addMarker(locLat, locLng, locTitle) {
         title: locTitle,
         map: map
     });
-    markers.push(marker);
 };
 
-//Clears markers on map
-function clearMarkers() {
-    setMapOnAll(null);
-    markers = [];
-}
-
-// Sets the map on all markers in the array.
-function setMapOnAll(map) {
-    for (var i = 0; i < markers.length; i++) {
-        markers[i].setMap(map);
-    }
-}
-
-
+//Renders the map based off the string of a location
 function displayMap(LocationString) {
-    console.log("In displayMap");
     // Get the latitude and longitude of the Address from the Address String passes in.
     $.get("https://maps.googleapis.com/maps/api/geocode/json?address=" + LocationString + "&key=AIzaSyCSCJJqp5sR0Q6TMspPZ_SeqnTC7iLPnmE")
         .done(function (data) {
             var address = JSON.stringify(data);
-            console.log("Results - " + address);
-            console.log("Data Lat- " + data.results[0].geometry.location.lat);
-            console.log("Data Lng- " + data.results[0].geometry.location.lng);
-            console.log("Short Name- " + data.results[0].address_components.short_name);
             addressLng = data.results[0].geometry.location.lng;
             addressLat = data.results[0].geometry.location.lat;
-            addrMarker = data.results[0].address_components.short_name;
             addressFormatted = data.results[0].formatted_address;
             // Create the map with the lat/lng retrieved from the API.
             map = new google.maps.Map(document.getElementById('map'), {
-                zoom: 13,
+                zoom: 16,
                 center: { lat: Number(addressLat), lng: Number(addressLng) },
             });
-            addMarker(addressLat, addressLng, addrMarker);
+            addMarker(addressLat, addressLng, LocationString);
             $("#galleryName").text(LocationString);
             $("#galleryAddr").text(addressFormatted);
         });
 }
 
+//For the sticky nav
 $(window).scroll(function () {
-    if ($(window).scrollTop() >= 80) {
+    if ($(window).scrollTop() >= 63) {
         $("#index-banner").addClass('fixed-header');
     }
     else {
@@ -82,6 +71,9 @@ $(window).scroll(function () {
 });
 
 $(document).ready(function () {
+
+    //Allows modals to be triggered
+    initializeModal();
 
     ////////ARTSY API//////////
 
@@ -114,16 +106,20 @@ $(document).ready(function () {
     //On click listener for arist search
     $("#submit").on("click", function (event) {
 
+        emptyCard();
+
+        emptyCarousel();
+
         event.preventDefault();
 
         imageArray = []
 
-        getArtistName();
-
+        getArtistId();
 
     })
 
-    function getArtistName() {
+    //Queries the API to find the ID of the artist
+    function getArtistId() {
 
         artistName = $("#artist-name").val().trim()
 
@@ -134,18 +130,21 @@ $(document).ready(function () {
             method: 'GET',
             beforeSend: function (xhr) { xhr.setRequestHeader('X-Xapp-Token', xappToken); }
         }).done(function (data) {
-            console.log(data)
 
             var artistId = data._embedded.results.filter(function (item) {
                 return item.type === "artist";
             })[0]._links.self.href.split("/").pop();
 
             artistIdQueryFunc(artistId)
-            console.log(artistId)
+
+            artistName = data._embedded.results.filter(function (item) {
+                return item.type === "artist";
+            })[0].title
         })
 
     }
 
+    //Queries the API based on the artist ID, returns a list of all public artworks
     function artistIdQueryFunc(id) {
         var artistIdQuery = url + "artworks?artist_id=" + id
         $.ajax({
@@ -153,21 +152,22 @@ $(document).ready(function () {
             method: 'GET',
             beforeSend: function (xhr) { xhr.setRequestHeader('X-Xapp-Token', xappToken); }
         }).done(function (results) {
-            console.log(results)
+
+            if (results._embedded.artworks.length == 0) {
+                noArtFound();
+            }
 
             for (var i = 0; i < results._embedded.artworks.length; i++) {
 
                 var arrayId = results._embedded.artworks[i].id
 
-                // collectingInstitution.push(results._embedded.artworks[i].collecting_institution)
-
                 getArtwork(arrayId, i)
             }
-
 
         })
     }
 
+    //Takes each artwork id, takes the details of each piece, adds to an object whcih is then pushed to the array
     function getArtwork(id, counter) {
         var artworkQuery = url + "/artworks/" + id
         $.ajax({
@@ -176,13 +176,13 @@ $(document).ready(function () {
             beforeSend: function (xhr) { xhr.setRequestHeader('X-Xapp-Token', xappToken); }
         }).done(function (results) {
 
-            console.log(results)
-
             var image = results._links.thumbnail.href
             var collectingInstitution = results.collecting_institution
             var id = results.id
             var medium = results.medium
             var title = results.title
+            var date = results.date
+            var category = results.category
 
             imageArray.push({
                 arrayId: counter,
@@ -190,24 +190,33 @@ $(document).ready(function () {
                 imgUrl: image,
                 title: title,
                 institution: collectingInstitution,
-                medium: medium
+                medium: medium,
+                date: date,
+                category: category
 
             })
-            // console.log("image is " + image);
-
-            console.log(imageArray)
 
             renderCarousel(imageArray)
 
-        })
+            renderImage(imageArray[0].imgUrl, imageArray[0].title)
 
+            if (!(imageArray[0].institution)) {
+                resetMap();
+            }
+            else {
+                displayMap(imageArray[0].institution);
+            }
+
+            renderContent(artistName, imageArray[0].title, imageArray[0].category, imageArray[0].date, imageArray[0].medium, imageArray[0].imgUrl)
+
+        })
 
     }
 
-
+    //Takes in the array of images and renders the images to the carousel
     function renderCarousel(array) {
 
-        $(".carousel").empty()
+        emptyCarousel();
 
         $(array).each(function (key, value, ) {
 
@@ -215,6 +224,8 @@ $(document).ready(function () {
                 .attr("src", value.imgUrl)
                 .attr("id", "carousel-image")
                 .attr("data", value.arrayId)
+                .css("width", "100%")
+                .css("height", "100%")
 
 
             var aDiv = $("<a>")
@@ -233,7 +244,7 @@ $(document).ready(function () {
             $(".carousel").removeClass("initialized")
 
         }
-        $('.carousel').carousel();
+        $('.carousel').carousel({padding: 50}, {shift: 50}, {dist: 0});
     }
 
     $(".carousel").on("click", "#carousel-image", function () {
@@ -243,7 +254,9 @@ $(document).ready(function () {
 
         var artLocation;
         var artTitle;
+        var artCategory;
         var artMedium;
+        var artDate;
 
         for (var i = 0; i < imageArray.length; i++) {
 
@@ -251,51 +264,74 @@ $(document).ready(function () {
                 artLocation = imageArray[i].institution
                 artTitle = imageArray[i].title
                 artMedium = imageArray[i].medium
+                artDate = imageArray[i].date
+                artCategory = imageArray[i].category
             }
-
         }
 
-        renderImage(imgSrc, artTitle, artMedium)
-        displayMap(artLocation)
+        renderImage(imgSrc, artTitle)
+        if (!(artLocation)) {
+            resetMap();
+        }
+        else {
+            displayMap(artLocation);
+        }
+
+        renderContent(artistName, artTitle, artCategory, artDate, artMedium, imgSrc)
 
     })
 
-    function renderImage(img, title, medium) {
+    function renderImage(img, title) {
 
-        $("#card-image").empty();
+        emptyCard();
 
-        var imgDiv = $("<img>").attr("src", img)
-        var smallerImg = $('<img>').attr('src', img)
+        var imgDiv = $("<img>")
+                    .attr("src", img)
 
         $("#card-image").html(imgDiv)
-        $("#infoPic").html(smallerImg);
 
-        $("#art-title").text(title)
-        $("#link").html('<a href=https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Daps&field-keywords=' + "fine%20art%20" + encodeURI(title) + "%20" + encodeURI(artistName) + '>Find on Amazon</a>')
+        $("#art-title").html(title + '<i class="material-icons right">more_vert</i>')
+        $("#link").html('<a href=https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Daps&field-keywords=' + "fine%20art%20" + encodeURI(title) + "%20" + encodeURI(artistName) + ' target="_blank">Find on Amazon</a>')
 
     }
 
-    //queries API for artist ID
+    function renderContent(name, title, category, date, medium, img) {
+        $(".card-reveal").empty();
 
-    //Create API pull based off artist name
+        var wrapperDiv = $("<div>")
+        var imgDiv = $("<img>").attr("src", img)
 
-    //Create array of images with properties of location
+        wrapperDiv.append(imgDiv);
 
-    //Render images to slider div
+        wrapperDiv.append(
+            '<span class="card-title grey-text text-darken-4">' + title + '<i class="material-icons right">close</i></span>' +
+            '<div>' +
+            '<p>' + "Category: " + category + '</p>' +
+            '<p>' + "Medium: " + medium + '</p>' +
+            '<p>' + "Date Created: " + date + '</p>' +
+            '</div>'
+        )
 
-    //On click listener for image
+        $(".card-reveal").append(wrapperDiv);
+    }
 
+    function emptyCard() {
+        $("#card-image").empty();
+    }
 
-    //Use Google Maps API to show location
+    function initializeModal() {
+        $('.modal').modal();
+    }
 
-    //Query API to convert location to coordinates
+    function noArtFound() {
+        $('#modal1').modal('open')
+        $('#modal1').css("z-index", 99999)
+    }
 
-    //render Google maps with current location
-
-    //Render image to lefthand div
+    function emptyCarousel() {
+        $(".carousel").empty()
+    }
 
     initializeQuery();
-
-
 
 })
